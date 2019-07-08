3c85251a29971fc8bdcd3b19878fc2239d1e9047
Sumanyu Ghoshal <51703486+sumanyug@users.noreply.github.com>
2019-07-08 13:53:51 +0530

    Add files via upload

---
+++ sendvid.py
@@ -0,0 +1,84 @@
+import numpy as np
+import cv2
+import time
+import smtplib
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+def recordvid():
+    # The duration in seconds of the video captured
+    capture_duration = 5
+
+    # InputCamera
+    cap = cv2.VideoCapture(0)
+
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    out = cv2.VideoWriter('vid.avi', fourcc, 20.0, (640, 480))
+
+    start_time = time.time()
+    while(int(time.time() - start_time) < capture_duration):
+        ret, frame = cap.read()
+        if ret == True:
+            frame = cv2.flip(frame, 0)
+            out.write(frame)
+            cv2.imshow('frame', frame)
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+def send_mail():
+    now = datetime.now()
+    msg = MIMEMultipart()
+    msg["From"] = "techchefofficial@gmail.com"
+    msg["To"] = "techchefsecurityas@gmail.com"
+    msg["subject"] = "Intruder Alert!"
+    message = " An intruder has been noticed at {}" % now
+    msg.attach(MIMEText(message, 'plain'))
+    filename = "vid.avi"
+    attachment = open("vid.avi", "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= {}" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(str(msg["From"]), "IITBombay")
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(str(msg["From"]), str(msg["To"]), text)
+
+    # terminating the session
+    s.quit()
+
+
+def Main_vid(condition):
+    if not condition:
+        recordvid()
+        send_mail()
